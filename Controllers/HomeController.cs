4e65739448fb2567b592338cfe690b090dd4755b
Nessa-Morais <nessa_mengo@hotmail.com>
2022-06-26 20:36:44 -0300

    hotsite

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            DatabaseService dbs = new DatabaseService();
+           Interesse interesseId = new Interesse();
+
+           
+            if(cad.Nome == null){
+                ViewBag.msg = "O campo nome não foi preenchido";
+                               
+            } else{
+                if(cad.Email == null){
+                      ViewBag.msg = "O campo E-mail não foi preenchido";
+                
+                }else{
+                    
+                    interesseId.Id = dbs.CadastraInteresse(cad);
+
+                       
+                    if(interesseId.Id > 0){
+                    ViewBag.msg = "Cadastro realizado com sucesso!";
+                
+                    }else{
+                       ViewBag.msg = "Falha no cadastro, tente mais tarde!";
+                    }
+                }
+            }
+            return View("Index",cad);
+        }
+
+    }
+}
